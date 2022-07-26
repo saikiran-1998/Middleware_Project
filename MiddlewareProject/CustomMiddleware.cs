981c8f6fb946679c7938d23ce5ec09a2fa4ba5ef
Saikiran Annangi <Saikiran_Annangi@epam.com>
2022-07-27 00:29:50 +0530

    Add project files.

---
+++ MiddlewareProject/CustomMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddlewareProject
+{
+    internal class CustomMiddleware
+    {
+        public RequestDelegate next;
+        public string environment;
+        public CustomMiddleware(RequestDelegate next, IWebHostEnvironment environment)
+        {
+            this.next = next;
+            this.environment = environment.EnvironmentName;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync("this is " + environment+" Environment");
+            await this.next(context);
+        }
+    }
+}
